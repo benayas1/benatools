b2130d060d6844112a9b55e3caf898bf71973fa8
Benayas <benayas1@gmail.com>
2020-07-19 12:58:51 +0200

    pytorch efficiennet

---
+++ benatools/torch/efficient_net.py
@@ -0,0 +1,8 @@
+import efficientnet.tfkeras as efn
+
+EFNS = [efn.EfficientNetB0, efn.EfficientNetB1, efn.EfficientNetB2, efn.EfficientNetB3,
+        efn.EfficientNetB4, efn.EfficientNetB5, efn.EfficientNetB6, efn.EfficientNetB7]
+
+def create_efn(b, dim=128, weights='imagenet', include_top=False):
+        base = EFNS[b](input_shape=(dim, dim, 3), weights=weights, include_top=include_top)
+        return base
