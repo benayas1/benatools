a28906e617e67ca3043f2cc4b5a083ab5c809e8d
albertobenayas <45709597+albertobenayas@users.noreply.github.com>
2020-08-30 13:47:01 +0200

    Created GBMOptimizer class
    Created GBMOptimizer class

--- benatools/gbm/opt.py
+++ benatools/gbm/opt.py
@@ -107,8 +107,6 @@ def opt_catboost(X_train,
                        'loss_function': 'RMSE',
                        'eval_metric': 'RMSE',
                        'max_bin': hp.qloguniform('max_bin', np.log(32), np.log(64), 5),  # border count
-                       'od_type': 'Iter',  # 'IncToDec',  # 'Iter'
-                       'od_wait': early_stopping,
                        'task_type': device # GPU devices
                         # 'devices':0 # GPU device ID
                        }
@@ -411,7 +409,7 @@ def opt_lightgbm(X_train,
     return best
 
 
-def _get_params(library, early_stopping=None, device='GPU'):
+def _get_params(library, device='GPU'):
     if library == 'CB':
         return {'learning_rate': hp.loguniform('learning_rate', np.log(0.01), np.log(0.3)),
                 'random_state': hp.choice('random_state', [0, 1, 2, 3]),
@@ -423,8 +421,6 @@ def _get_params(library, early_stopping=None, device='GPU'):
                 'loss_function': 'RMSE',
                 'eval_metric': 'RMSE',
                 'max_bin': hp.qloguniform('max_bin', np.log(32), np.log(64), 5),  # border count
-                'od_type': 'Iter',  # 'IncToDec',  # 'Iter'
-                'od_wait': early_stopping,
                 'task_type': device # GPU devices
                 # 'devices':0 # GPU device ID
                 }, ['depth', 'max_bin', 'random_state']
@@ -464,10 +460,6 @@ def _get_params(library, early_stopping=None, device='GPU'):
                 # 'gpu_id': 0,
                 'device_type': device.lower()
                 }, ['max_depth', 'seed', 'bagging_freq','num_leaves','min_data_in_leaf']
-
-
-def _call_cv(library, params, int_params):
-    return
 
 
 def opt(X_train,
@@ -575,11 +567,228 @@ def opt(X_train,
 
 
 class GBMOptimizer:
-    def __init__(self, library, device=None, early_stopping=None, override_params=None):
+    def __init__(self, library, device='GPU', override_params=None):
         self.library = library
 
-        self.training_params, self.int_params = _get_params(library, early_stopping, device)
+        self.training_params, self.int_params = _get_params(library, device)
         self.training_params = _update_params(self.training_params, override_params)
 
-    def optimize(self):
-        return
+    def optimize(self,
+                 X_train,
+                 y_train,
+                 objective=None,
+                 cat_features=None,
+                 verbose=1,
+                 max_evals=100,
+                 max_rounds=5000,
+                 early_stopping=50,
+                 savepath=None):
+
+        # Build objective function
+        if objective is None:
+            objective = self._get_obj(X_train, y_train, cat_features, max_rounds, early_stopping)
+
+        # Run the optimization
+        self.trials = Trials()
+        hyperopt_f = fmin(fn=objective,
+                          space=self.training_params,
+                          algo=tpe.suggest,
+                          verbose=True if verbose > 0 else False,
+                          max_evals=max_evals,
+                          trials=self.trials)
+
+        # Extract the best params
+        best_params, best_n = self._get_best_params(hyperopt_f, self.trials)
+
+        # Builds a dict containing the best params and the best n iterations
+        best = {'params': best_params, 'n': best_n}
+
+        # Save results to json file
+        if savepath:
+            self._save_json(best, savepath)
+
+        return best
+
+    def _get_best_params(self, best, trials):
+        """
+        """
+        best_params = best.copy()
+        for par_name in self.int_params:
+            if par_name in best_params:
+                best_params[par_name] = int(best_params[par_name])
+        for key in self.training_params:
+            if key not in best_params.keys():
+                best_params[key] = self.training_params[key]
+        best_n_iterations = trials.best_trial['result']['best_n_iters'] if 'best_n_iters' in trials.best_trial['result'] else None
+        return best_params, best_n_iterations
+
+    def plot(self, best='min', invert=False, figsize=(15, 6)):
+        if not self.trials:
+            return
+        losses = self.trials.losses()
+
+        if invert:
+            losses = [x * -1 for x in losses]
+        x = np.arange(0, len(losses))
+        ys = lowess(losses, x)[:, 1]
+
+        best_iteration = np.argmax(losses) if best == 'max' else np.argmin(losses)
+
+        best_value = losses[best_iteration]
+
+        plt.figure(figsize=figsize)
+        plt.plot(x, ys, 'red', linewidth=3)
+        plt.plot(x, losses, 'o-')
+        plt.plot([best_iteration], [best_value], marker='o', markersize=10, color="red")
+        plt.suptitle('Evaluation summary', fontweight='bold', fontsize=20)
+        plt.title('There are %d evaluations, the best iteration is %d and the loss is %0.4f' % (
+                    len(losses), best_iteration, best_value))
+        plt.show()
+
+    def _save_json(self, data, path, verbose=1):
+        if path[path.rfind('.'):] != '.json':
+            path = path + '.json'
+        with open(path, 'w') as fp:
+            json.dump(data, fp)
+        if verbose > 0:
+            print('Best parameters saved to ' + path)
+
+    def _get_obj(self, X_train, y_train, cat_features=None, n_folds=5, folds=None, max_rounds=5000, early_stopping=50, seed=0, verbose=1):
+        if self.library == 'CB':
+            return self._get_obj_CB(X_train, y_train, cat_features, n_folds, folds, max_rounds, early_stopping, seed, verbose)
+        if self.library == 'XGB':
+            return self._get_obj_XGB(X_train, y_train, cat_features, n_folds, folds, max_rounds, early_stopping, seed, verbose)
+        if self.library == 'LGB':
+            return self._get_obj_LGB(X_train, y_train, cat_features, n_folds, folds, max_rounds, early_stopping, seed, verbose)
+
+    def _get_obj_CB(self, X_train, y_train, cat_features=None, n_folds=5, folds=None, max_rounds=5000, early_stopping=50, seed=0, verbose=1):
+        # Catboost dataset
+        train_pool = cb.Pool(data=X_train, label=y_train, cat_features=cat_features)
+
+        # If folds already provided, set cv_folds to None
+        if folds:
+            cv_folds = None
+
+        # Objective function for Hyperopt
+        def objective_f(params):
+            start = time.time()
+
+            # Control Integer Params don't go float
+            for par_name in self.int_params:
+                params[par_name] = int(np.round(params[par_name]))
+
+            cv = cb.cv(pool=train_pool,
+                       params=params,
+                       iterations=max_rounds,
+                       nfold=n_folds,
+                       inverted=False,
+                       shuffle=True,
+                       verbose=False,
+                       seed=seed,
+                       early_stopping_rounds=early_stopping,
+                       folds=folds,
+                       as_pandas=True)
+
+            # Metric to extract the loss from
+            test_loss = 'test-RMSE-mean'
+            train_loss = 'train-RMSE-mean'
+            best_iteration = cv[test_loss].idxmin()
+            test_loss_value = cv[test_loss].iloc[best_iteration]
+            train_loss_value = cv[train_loss].iloc[best_iteration]
+
+            if verbose > 0:
+                print('Train Loss: %0.4f, Test Loss: %0.4f RMSE with %d iterations. Time elapsed %s' % (
+                    train_loss_value, test_loss_value, best_iteration, str(round(time.time() - start, 2))))
+
+            val = {'loss': test_loss_value,  # mandatory
+                   'status': STATUS_OK,  # mandatory
+                   'best_n_iters': best_iteration}
+
+            return val
+
+        return objective_f
+
+    def _get_obj_XGB(self, X_train, y_train, cat_features=None, n_folds=5, folds=None, max_rounds=5000, early_stopping=50, seed=0, verbose=1):
+
+        # Transform categorical features to one_hot
+        if cat_features:
+            X_train = ce.one_hot.OneHotEncoder(cols=cat_features, drop_invariant=True).fit_transform(X_train)
+        train_dmatrix = xgb.DMatrix(X_train, y_train)
+
+        def objective_f(params):
+            start = time.time()
+            # Control Integer Params don't go float
+            for par_name in self.int_params:
+                params[par_name] = int(np.round(params[par_name]))
+
+            cv = xgb.cv(params=params,
+                        dtrain=train_dmatrix,
+                        nfold=n_folds,
+                        folds=folds,
+                        stratified=False,
+                        metrics=params['eval_metric'],
+                        verbose_eval=False,
+                        seed=seed,
+                        num_boost_round=max_rounds,
+                        as_pandas=True,
+                        early_stopping_rounds=early_stopping)
+
+            # Metric to extract the loss from
+            test_loss = 'test-rmse-mean'
+            train_loss = 'train-rmse-mean'
+            best_iteration = cv[test_loss].idxmin()
+            test_loss_value = cv[test_loss].iloc[best_iteration]
+            train_loss_value = cv[train_loss].iloc[best_iteration]
+
+            if verbose > 0:
+                print('Train Loss: %0.4f, Test Loss: %0.4f RMSE with %d iterations. Time elapsed %s' % (
+                    train_loss_value, test_loss_value, best_iteration, str(round(time.time() - start, 2))))
+
+            val = {'loss': test_loss_value,  # mandatory
+                   'status': STATUS_OK,  # mandatory
+                   'best_n_iters': best_iteration}
+
+            return val
+
+        return objective_f
+
+    def _get_obj_LGB(self, X_train, y_train, cat_features=None, n_folds=5, folds=None, max_rounds=5000, early_stopping=50, seed=0, verbose=1):
+        def objective_f(params):
+            start = time.time()
+
+            dataset = lgb.Dataset(X_train, label=y_train, categorical_feature=cat_features)
+
+            # Control Integer Params don't go float
+            for par_name in self.int_params:
+                params[par_name] = int(np.round(params[par_name]))
+
+            cv = lgb.cv(params=params,
+                        train_set=dataset,
+                        nfold=n_folds,
+                        folds=folds,
+                        stratified=False,
+                        verbose_eval=False,
+                        seed=seed,
+                        num_boost_round=max_rounds,
+                        early_stopping_rounds=early_stopping)
+
+            # Metric to extract the loss from
+            cv_results = pd.DataFrame(cv)
+            metric_mean_name = 'rmse-mean'
+            best_iteration = cv_results[metric_mean_name].idxmin()
+            loss = cv_results[metric_mean_name].iloc[best_iteration]
+
+            if verbose > 0:
+                print('Test Loss: %0.4f RMSE with %d iterations. Time elapsed %s' % (
+                    loss, best_iteration, str(round(time.time() - start, 2))))
+
+            val = {'loss': loss,  # mandatory
+                   'status': STATUS_OK,  # mandatory
+                   'best_n_iters': best_iteration}
+
+            return val
+        return objective_f
+
+
+
+
