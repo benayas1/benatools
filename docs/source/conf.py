4e7135803941405670966614bc3db34cbf7946df
Benayas <benayas1@gmail.com>
2020-10-16 19:26:07 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -47,6 +47,9 @@ templates_path = ['_templates']
 exclude_patterns = []
 #autosummary_generate = True
 
+
+autodoc_mock_imports = ["xgboost"]
+
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
