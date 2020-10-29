663b16d00b34150b3b71c5f99f4c8516fbd32eb7
Alberto Benayas <benayas1@gmail.com>
2020-10-29 20:01:57 +0000

    fitter

--- src/benatools/torch/fitter.py
+++ src/benatools/torch/fitter.py
@@ -105,13 +105,6 @@ class TorchFitter:
         self.model = model
         self.device = device
 
-        param_optimizer = list(self.model.named_parameters())
-        no_decay = ['bias', 'LayerNorm.bias', 'LayerNorm.weight']
-        optimizer_grouped_parameters = [
-            {'params': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)], 'weight_decay': 0.001},
-            {'params': [p for n, p in param_optimizer if any(nd in n for nd in no_decay)], 'weight_decay': 0.0}
-        ]
-
         # Optimizer object
         self.optimizer = optimizer
 
